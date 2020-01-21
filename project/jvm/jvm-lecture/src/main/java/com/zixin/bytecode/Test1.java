69961d7db3dd0aee6b09d34276f73dd06465bd2d
AndyFlower <1308445442@qq.com>
2020-01-21 17:21:14 +0800

    20200121

---
+++ project/jvm/jvm-lecture/src/main/java/com/zixin/bytecode/Test1.java
@@ -0,0 +1,20 @@
+package com.zixin.bytecode;
+
+/**
+ * @ClassName Test1
+ * @Description TODO
+ * @Author zixin
+ * @Date 2020/1/20 10:48
+ * @Version 1.0
+ **/
+public class Test1 {
+    private int a=1;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
