519cb55cbe91b0c82dbe7c91291b79e732fc9d98
念念 <3410766560@qq.com>
2020-03-24 23:49:34 +0800

    20200324

--- project/Pat/learn/src/main/java/com/zixin/learn/nnuo/AnyToTen.java
+++ project/Pat/learn/src/main/java/com/zixin/learn/nnuo/AnyToTen.java
@@ -16,5 +16,23 @@ public class AnyToTen {
 		System.out.println(ans);
 
 	}
+	
+	
+	public static void AnyToTen(String[] args) {
+		char s[] = new char[105];//保存任何进制的字符串
+		int ans = 0 ;
+		int len = s.length;
+		int x = 16;//x表示是几进制
+		for(int i=0;i<len;i++) {
+			ans = ans *x;
+			if(s[i]>='0'&&s[i]<='9') {
+				ans = ans+(s[i]-'0');
+			}else {
+				ans = ans+(s[i]-'A')+10;
+			}
+		}
+		System.out.println(ans);
+
+	}
 
 }
